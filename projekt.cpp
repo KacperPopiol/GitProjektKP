49c1ddfdd7a83b1da1b0117d1f817d2ce1447fe3
Janusz Nowak <janusz.nowak@pwsz-ns.edu.pl>
2022-10-24 11:35:57 +0200

    v.1

--- projekt.cpp
+++ projekt.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    int a,b,x;
+    cout <<"Podaj a i b funkcji liniowej: ";
+    cin >> a >> b;
+    x = (-b)/a;
+    cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+
+    return 0;
+}
