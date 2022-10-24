23a945894b82eb415546e6410906ff70faa71d99
Janusz Nowak <janusz.nowak@pwsz-ns.edu.pl>
2022-10-24 12:10:39 +0200

    v.1.4

--- projekt.cpp
+++ projekt.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
-    int a,b,x;
-    cout <<"Podaj a i b funkcji liniowej: ";
-    cin >> a >> b;
-    if(a != 0)
+    int A_,B_,C_,a,b,x,n;
+    cout <<"Wybierz postac funkcji: "<<endl;
+    cout <<"1. y=ax+b: "<<endl;
+    cout <<"2. Ax+By+C=0: "<<endl;
+    cin >> n;
+
+    switch(n)
     {
-        x = (-b)/a;
-        cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+      case 1:
+            system("cls");
+                cout <<"Podaj a i b funkcji liniowej: ";
+            cin >> a >> b;
+            if(a != 0)
+            {
+                x = (-b)/a;
+                cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+            }
+            else if(a == 0)
+            {
+                cout<<"Fukcja jest stala, brak miejsc zerowych";
+            }
+        break;
+      case 2:
+           system("cls");
+            cout <<"Podaj A, B, C funkcji liniowej: ";
+            cin >> A_ >> B_ >> C_;
+            a = (-A_)/B_;
+            b = (-C_)/B_;
+
+            if(a != 0)
+            {
+                x = (-b)/a;
+                cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+            }
+            else if(a == 0)
+            {
+                cout<<"Fukcja jest stala, brak miejsc zerowych";
+            }
+
+        break;
+      default:
+        cout << "Nie ma takiej opcji";
+        return 1;
     }
-    else if(a == 0)
-    {
-        cout<<"Fukcja jest stala, brak miejsc zerowych";
-    }
+
+
+
 
 
     return 0;
