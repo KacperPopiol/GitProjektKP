7b410875e138c14c6b1df31dec168e0b5d064c57
Kacper Popiol <adam.nowak@pwsz-ns.edu.pl>
2022-11-07 11:10:49 +0100

    2.3

--- projekt.cpp
+++ projekt.cpp
@@ -1,57 +1,97 @@
 #include <iostream>
 #include <windows.h>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-    int A_,B_,C_,a,b,x,n;
-    cout <<"Wybierz postac funkcji: "<<endl;
-    cout <<"1. y=ax+b: "<<endl;
-    cout <<"2. Ax+By+C=0: "<<endl;
-    cin >> n;
+    int A_,B_,C_,a,b,x,n,m;
+    cout <<"Wybierz funkcje: "<<endl;
+    cout <<"[1] - Funkcja liniowa "<<endl;
+    cout <<"[2] - Funkcja kwadratowa "<<endl;
+    cin >> m;
 
-    switch(n)
+    switch(m)
     {
-      case 1:
+        case 1:
             system("cls");
-                cout <<"Podaj a i b funkcji liniowej: ";
-            cin >> a >> b;
-            if(a != 0)
+            cout <<"Wybierz postac funkcji: "<<endl;
+            cout <<"1. y=ax+b: "<<endl;
+            cout <<"2. Ax+By+C=0: "<<endl;
+            cin >> n;
+
+            switch(n)
             {
-                x = (-b)/a;
-                cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+              case 1:
+                    system("cls");
+                        cout <<"Podaj a i b funkcji liniowej: ";
+                    cin >> a >> b;
+                    if(a != 0)
+                    {
+                        x = (-b)/a;
+                        cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+                    }
+                    else if(a == 0)
+                    {
+                        cout<<"Fukcja jest stala, brak miejsc zerowych";
+                    }
+                break;
+              case 2:
+                   system("cls");
+                    cout <<"Podaj A, B, C funkcji liniowej: ";
+                    cin >> A_ >> B_ >> C_;
+                    a = (-A_)/B_;
+                    b = (-C_)/B_;
+
+                    if(a != 0)
+                    {
+                        x = (-b)/a;
+                        cout <<"Miejsce zerowe podanej funkcji to: "<<x;
+                    }
+                    else if(a == 0)
+                    {
+                        cout<<"Fukcja jest stala, brak miejsc zerowych";
+                    }
+                    break;
+
+              default:
+                cout<<"Nie ma takiej opcji w menu";
+                return 1;
+                break;
             }
-            else if(a == 0)
-            {
-                cout<<"Fukcja jest stala, brak miejsc zerowych";
-            }
-        break;
-      case 2:
-           system("cls");
-            cout <<"Podaj A, B, C funkcji liniowej: ";
-            cin >> A_ >> B_ >> C_;
-            a = (-A_)/B_;
-            b = (-C_)/B_;
+            break;
+            case 2:
+                system("cls");
+                float a,b,c,d,x,x1,x2;
+                cout<<"Podaj wspolczynnik kierunkowy prostej a "<<endl;
+                cin>>a;
+                cout<<"Podaj wspolczynnik b "<<endl;
+                cin>>b;
+                cout<<"Podaj wspolczynnik c "<<endl;
+                cin>>c;
+                d=(b*b)-(4*a*c);
+                if(d>0)
+                {
+                    x1=(-b-sqrt(d))/(2*a);
+                    x2=(-b+sqrt(d))/(2*a);
+                    cout<<"Wynik to : x1 = "<<x1<<", x2 = "<<x2;
+                }
+                else if(d==0)
+                {
+                    x=-b/(2*a);
+                    cout<<"Wynik to : x = "<<x;
+                }
+                else if(d<0)  // Jeœli delta mniejsza od "0".
+                {
+                        cout<<"Brak rozwiazan";
+                    }
+                          break;
 
-            if(a != 0)
-            {
-                x = (-b)/a;
-                cout <<"Miejsce zerowe podanej funkcji to: "<<x;
-            }
-            else if(a == 0)
-            {
-                cout<<"Fukcja jest stala, brak miejsc zerowych";
-            }
+                        default:
+                            cout<<"Nie ma takiej opcji w menu";
+                            break;
 
-        break;
-      default:
-        cout << "Nie ma takiej opcji";
-        return 1;
-    }
-
-
-
-
+                }
 
     return 0;
 }
